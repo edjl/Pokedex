f507837f0581aaf98d9edbbd0de9b309458bdc55
edlee <edlee05072002@gmail.com>
2022-05-24 10:52:59 -0400

    Security Update.

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -7,7 +7,7 @@ public interface Table {
     static final String Driver = "com.mysql.jdbc.Driver";
     static final String database = "jdbc:mysql://localhost/POKEMON";
     static final String user = "root";
-    static final String pass = "Cindylee@2";
+    static final String pass = "";
     public static void updateTable() {}
     public static String handleApostraphe(String line) {
         int index = 0;
