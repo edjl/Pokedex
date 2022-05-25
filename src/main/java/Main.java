5a0aac7cf1f210f60a50119fdc723b9c885fe4cb
edlee <edlee05072002@gmail.com>
2022-05-25 18:54:06 -0400

    Set up Print for Pokemon Details

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -67,9 +67,6 @@ public class Main {
     }
 
     public static void main (String[] args) {
-        Pokemon p = new Pokemon("Latios", 50, 20, 20, "adamant");
-        p.printPokemon();
-
         Scanner sc = new Scanner (System.in);
         System.out.println("Welcome to Edward's Pokedex!");
         char prompt = initialPrompt(sc);
